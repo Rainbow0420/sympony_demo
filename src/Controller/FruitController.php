fd637c9714949cf4376d8ff0af47c273fbe7a29b
KingTiger <KingTiger@gmail.com>
2023-04-22 00:29:26 +0900

    add

---
+++ src/Controller/FruitController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class FruitController extends AbstractController
+{
+    #[Route('/fruit', name: 'app_fruit')]
+    public function index(): Response
+    {
+        return $this->render('fruit/index.html.twig', [
+            'controller_name' => 'FruitController',
+        ]);
+    }
+}
